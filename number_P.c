b1af412ade884b41f0da9b05a1a3f6af26b6bc21
bitterthanbit <pjp040911@gmail.com>
2018-04-02 21:31:25 +0900

    NumberPyramid

---
+++ number_P.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void main()
+{
+	int n;
+	int i, j;
+	int pramid[500][500];
+	
+	scanf("%d",&n);
+	for(i = 0; i < n; i++) {
+		for(j = 0; j < i+1; j++)
+			scanf("%d",&pramid[i][j]);
+	}
+	
+	for(i = n-2; i >= 0; i--) {
+		for(j = 0; j < i+1; j++) {
+			if(pramid[i+1][j] > pramid[i+1][j+1])
+				pramid[i][j] += pramid[i+1][j];
+			else
+				pramid[i][j] += pramid[i+1][j+1];
+		}
+	}
+	printf("%d\n",pramid[0][0]);
+}
